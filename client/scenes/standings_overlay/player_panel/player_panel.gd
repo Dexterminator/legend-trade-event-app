c0c30cf5be3526fa2c858a299f6858866ddee6ad
Dexter Gramfors <dexter.gramfors@gmail.com>
2026-04-04 18:39:07 -0400

    Add leaderboard assets

--- client/scenes/standings_overlay/player_panel/player_panel.gd
+++ client/scenes/standings_overlay/player_panel/player_panel.gd
@@ -1,10 +1,7 @@
 class_name PlayerPanel
 extends PanelContainer
 
-@onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var name_label: Label = %NameLabel
-@onready var score_label: Label = %ScoreLabel
-@onready var placement_label: Label = %PlacementLabel
 
 enum State {EXPANDED, COLLAPSED}
 var state: State = State.COLLAPSED
@@ -14,17 +11,20 @@ var ranking: int = 0
 var showing_score: int = 0
 var score_tween: Tween
 
+func init() -> void:
+	# Set name
+	# Set PFP
+	# Set country flag
+	# Set top pos
+	pass
+
+func _update() -> void:
+	# Set top pos
+	# Set trade volume
+	# Set PnL
+	# Draw sparkline
+	pass
+
 func set_pnl_pct(new_pnl_pct: float) -> void:
 	pnl_pct = new_pnl_pct
-	score_label.text = "%0.2f%%" % pnl_pct
-
-func toggle_expanded() -> void:
-	if animation_player.is_playing():
-		animation_player.stop()
-	match state:
-		State.EXPANDED:
-			animation_player.play("collapse")
-			state = State.COLLAPSED
-		State.COLLAPSED:
-			animation_player.play("expand")
-			state = State.EXPANDED
+	# score_label.text = "%0.2f%%" % pnl_pct
