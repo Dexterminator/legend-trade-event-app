bb39b0cce59a30fcaa5f0f0523cc72bb63ba1667
Dexter Gramfors <dexter.gramfors@gmail.com>
2026-04-04 22:14:11 -0400

    Set images on init

--- client/scenes/standings_overlay/player_panel/player_panel.gd
+++ client/scenes/standings_overlay/player_panel/player_panel.gd
@@ -34,8 +34,10 @@ func init(trader: Dictionary, new_rank: int) -> void:
 	user_name_label.text = trader["username"]
 	rank = new_rank
 	_update_rank_label()
-	# TODO: Set PFP (dict by username?)
-	# TODO: Set country flag (dict by username?)
+	var user_name: String = trader["username"]
+	var country_code: String = trader["country_code"]
+	pfp_texture.texture = load("res://assets/%s.png" % user_name.to_lower())
+	flag_texture.texture = load("res://assets/flag_%s.png" % country_code.to_lower())
 
 func _update(updates: Dictionary) -> void:
 	rank = updates["rank"]
