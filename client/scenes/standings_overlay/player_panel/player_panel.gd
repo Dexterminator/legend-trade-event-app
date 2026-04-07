19846966edc6c6b44fd38cd4df8c41083f728cb6
Dexter Gramfors <dexter.gramfors@gmail.com>
2026-04-06 21:09:48 -0400

    Fix

--- client/scenes/standings_overlay/player_panel/player_panel.gd
+++ client/scenes/standings_overlay/player_panel/player_panel.gd
@@ -2,6 +2,7 @@ class_name PlayerPanel
 extends PanelContainer
 
 const PlayerPanelSparklineScript := preload("res://scenes/standings_overlay/player_panel/player_panel_sparkline.gd")
+const SYMBOL_ICON_BASE_URL := "https://legend-trade-dev.s3.amazonaws.com/token-images/%s.png"
 
 @onready var user_name_label: Label = %UserNameLabel
 @onready var rank_container: HBoxContainer = %RankContainer
@@ -14,9 +15,14 @@ const PlayerPanelSparklineScript := preload("res://scenes/standings_overlay/play
 @onready var pnl_sparkline: PlayerPanelSparklineScript = %PnlSparkline
 @onready var rank_delta_indicator: TextureRect = %RankDeltaIndicator
 
+static var symbol_icon_cache: Dictionary = {}
+
 var rank_delta_sign: int = 0
 var ts_rank_delta_sign_changed: float = 0
 var is_eliminated: bool = false
+var _top_position_icon_slots: Array[TextureRect] = []
+var _symbol_icon_requests: Dictionary = {}
+var _current_top_position_symbols: Array[String] = []
 
 const RANK_DELTA_RESET_TIME: float = 3 * 60.0
 
@@ -36,6 +42,8 @@ func init(trader: Dictionary, new_rank: int) -> void:
 	rank = new_rank
 	_update_rank_label()
 	pnl_sparkline.clear_values()
+	_cache_top_position_slots()
+	_update_top_positions(_as_array(trader.get("top_positions", [])))
 	var user_name: String = trader["username"]
 	var country_code: String = trader["country_code"]
 	pfp_texture.texture = load("res://assets/%s.png" % user_name.to_lower())
@@ -51,12 +59,94 @@ func _update(updates: Dictionary) -> void:
 	volume_label.text = Utils.format_compact_number(volume)
 	is_eliminated = updates["is_eliminated"]
 	visible = not is_eliminated
+	_update_top_positions(_as_array(updates.get("top_positions", [])))
 
 	if Delta.exceeded(ts_rank_delta_sign_changed, RANK_DELTA_RESET_TIME):
 		rank_delta_sign = 0
 		rank_delta_indicator.texture = rank_delta_textures["neutral"]
 
-	# TODO: Set top pos
+
+func _cache_top_position_slots() -> void:
+	if not _top_position_icon_slots.is_empty():
+		return
+
+	for child in top_positions_container.get_children():
+		if child is TextureRect:
+			_top_position_icon_slots.append(child as TextureRect)
+
+
+func _update_top_positions(top_positions: Array) -> void:
+	_cache_top_position_slots()
+	_current_top_position_symbols.clear()
+
+	for i in _top_position_icon_slots.size():
+		var slot := _top_position_icon_slots[i]
+		if i >= top_positions.size():
+			slot.texture = null
+			continue
+
+		var top_position: Variant = top_positions[i]
+		if not top_position is Dictionary:
+			slot.texture = null
+			continue
+
+		var top_position_dict: Dictionary = top_position
+		var symbol_value: Variant = top_position_dict.get("symbol", "")
+		var symbol := str(symbol_value).to_upper()
+		if symbol.is_empty():
+			slot.texture = null
+			continue
+
+		_current_top_position_symbols.append(symbol)
+		_apply_symbol_icon(slot, symbol)
+
+
+func _apply_symbol_icon(slot: TextureRect, symbol: String) -> void:
+	if symbol_icon_cache.has(symbol):
+		slot.texture = symbol_icon_cache[symbol]
+		return
+
+	slot.texture = null
+	if _symbol_icon_requests.has(symbol):
+		return
+
+	var request := HTTPRequest.new()
+	_symbol_icon_requests[symbol] = request
+	add_child(request)
+	request.request_completed.connect(_on_symbol_icon_request_completed.bind(symbol, request))
+	var url_symbol := "HYPE" if symbol == "$HYPE" else symbol
+	var error := request.request(SYMBOL_ICON_BASE_URL % url_symbol)
+	if error != OK:
+		_symbol_icon_requests.erase(symbol)
+		request.queue_free()
+
+
+func _on_symbol_icon_request_completed(result: int, response_code: int, _headers: PackedStringArray, body: PackedByteArray, symbol: String, request: HTTPRequest) -> void:
+	_symbol_icon_requests.erase(symbol)
+	if is_instance_valid(request):
+		request.queue_free()
+
+	if result != HTTPRequest.RESULT_SUCCESS or response_code != 200:
+		return
+
+	var image := Image.new()
+	var error := image.load_png_from_buffer(body)
+	if error != OK:
+		return
+
+	var texture := ImageTexture.create_from_image(image)
+	symbol_icon_cache[symbol] = texture
+	for i in _top_position_icon_slots.size():
+		if i >= _current_top_position_symbols.size():
+			continue
+		if _current_top_position_symbols[i] == symbol:
+			_top_position_icon_slots[i].texture = texture
+
+
+func _as_array(value: Variant) -> Array:
+	if value is Array:
+		return value
+	return []
 
 func set_rank_label(new_rank: int) -> void:
 	var prev_rank: int = int(rank_label.text)
