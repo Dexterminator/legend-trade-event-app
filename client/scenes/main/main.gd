0372223067563ed51ed7e4d04e66954292a0b67b
Dexter Gramfors <dexter.gramfors@gmail.com>
2026-04-07 22:20:16 -0400

    top bar fix

--- client/scenes/main/main.gd
+++ client/scenes/main/main.gd
@@ -69,7 +69,7 @@ func _spawn_overlay() -> void:
 				key = kv[1].to_lower().strip_edges()
 				break
 	else:
-		key = "standings" # default for native/editor
+		key = "top_bar" # default for native/editor
 	if key in OVERLAYS:
 		var overlay: Control = (OVERLAYS[key] as PackedScene).instantiate()
 		overlay.focus_mode = Control.FOCUS_NONE
--- client/scenes/top_bar_overlay/top_bar_player/top_bar_player.gd
+++ client/scenes/top_bar_overlay/top_bar_player/top_bar_player.gd
@@ -5,11 +5,22 @@ class_name TopBarPlayer
 @onready var pnl_pct_label: Label = %PnlPctLabel
 @onready var flag_texture: TextureRect = %Flag
 
+var _is_eliminated := false
+
 func init(trader: Dictionary) -> void:
 	var country_code: String = trader["country_code"]
 	name_label.text = trader["username"]
 	flag_texture.texture = load("res://assets/flag_%s.png" % country_code.to_lower())
+	var is_eliminated: bool = trader.get("is_eliminated", false)
+	_apply_elimination_state(is_eliminated, false)
 
 func update(updates: Dictionary) -> void:
+	var is_eliminated: bool = updates.get("is_eliminated", false)
+	_apply_elimination_state(is_eliminated)
 	var pnl_pct: float = updates["pnl_pct"]
 	Utils.format_pct_label(pnl_pct_label, pnl_pct)
+
+
+func _apply_elimination_state(next_is_eliminated: bool, _animate: bool = true) -> void:
+	_is_eliminated = next_is_eliminated
+	visible = not _is_eliminated
