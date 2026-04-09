a2bfcba092ec1394385576dc4c6928463606880a
Dexter Gramfors <dexter.gramfors@gmail.com>
2026-04-08 22:12:52 -0400

    Add simple top bar

--- client/scenes/top_bar_overlay/top_bar_overlay.gd
+++ client/scenes/top_bar_overlay/top_bar_overlay.gd
@@ -1,14 +1,39 @@
 extends Control
 
+@onready var headers: VBoxContainer = $HBoxContainer/Headers
 @onready var players_container: HBoxContainer = %PlayersContainer
 
 var inited: bool = false
+var is_simple := false
 
 func _ready() -> void:
+	_apply_url_query_params()
+	_apply_display_mode()
 	modulate.a = 0.0
 	var t := create_tween()
 	t.tween_property(self , "modulate:a", 1.0, 2.0).set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_SINE)
 	SignalBus.standings_updated.connect(_on_standings_updated)
+
+
+func _apply_url_query_params() -> void:
+	if not OS.has_feature("web"):
+		return
+
+	var query: String = JavaScriptBridge.eval("window.location.search")
+	for part: String in query.trim_prefix("?").split("&"):
+		var kv := part.split("=")
+		if kv.size() != 2 or kv[0] != "simple":
+			continue
+
+		is_simple = kv[1].to_lower().strip_edges() == "true"
+		return
+
+
+func _apply_display_mode() -> void:
+	headers.visible = not is_simple
+	for i in range(players_container.get_child_count()):
+		var player_node := players_container.get_child(i) as TopBarPlayer
+		player_node.set_simple_mode(is_simple)
 
 func _init_top_bar_players(payload: Dictionary) -> void:
 	var traders: Array = payload["traders"]
--- client/scenes/top_bar_overlay/top_bar_player/top_bar_player.gd
+++ client/scenes/top_bar_overlay/top_bar_player/top_bar_player.gd
@@ -1,11 +1,14 @@
 extends VBoxContainer
 class_name TopBarPlayer
 
+@onready var name_panel: PanelContainer = $PanelContainer
+@onready var pnl_panel: PanelContainer = $PanelContainer2
 @onready var name_label: Label = %NameLabel
 @onready var pnl_pct_label: Label = %PnlPctLabel
 @onready var flag_texture: TextureRect = %Flag
 
 var _is_eliminated := false
+var _is_simple := false
 
 func init(trader: Dictionary) -> void:
 	var country_code: String = trader["country_code"]
@@ -21,6 +24,25 @@ func update(updates: Dictionary) -> void:
 	Utils.format_pct_label(pnl_pct_label, pnl_pct)
 
 
+func set_simple_mode(next_is_simple: bool) -> void:
+	_is_simple = next_is_simple
+	_set_panel_background_alpha(name_panel, 0.0 if _is_simple else 1.0)
+	_set_panel_background_alpha(pnl_panel, 0.0 if _is_simple else 1.0)
+	_apply_elimination_state(_is_eliminated, false)
+
+
+func _set_panel_background_alpha(panel: PanelContainer, alpha: float) -> void:
+	var panel_style := panel.get_theme_stylebox("panel")
+	if not panel_style is StyleBoxFlat:
+		return
+
+	var panel_style_copy := (panel_style as StyleBoxFlat).duplicate() as StyleBoxFlat
+	var bg_color := panel_style_copy.bg_color
+	bg_color.a = alpha
+	panel_style_copy.bg_color = bg_color
+	panel.add_theme_stylebox_override("panel", panel_style_copy)
+
+
 func _apply_elimination_state(next_is_eliminated: bool, _animate: bool = true) -> void:
 	_is_eliminated = next_is_eliminated
-	visible = not _is_eliminated
+	visible = true if _is_simple else not _is_eliminated
